b167073fb8bb436f9632fe26e2d11145d9f16140
yabushita <kazuhiko.yabushita@amd-lab.com>
2022-03-18 09:44:22 +0900

    CSS調整

--- hello-next/pages/index.tsx
+++ hello-next/pages/index.tsx
@@ -16,8 +16,7 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!!!!</a>
         </h1>
-        <p>Branch test</p>
-        <p>プルリクテストテスト</p>
+        <p>Next.jsが入りました!!!!!!</p>
 
         <p className={styles.description}>
           Get started by editing{' '}
